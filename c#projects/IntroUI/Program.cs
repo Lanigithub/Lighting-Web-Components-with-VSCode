20323c52a567d137ccfcc59bfa9f55bd2b4114be
Lani Fu <51423887+Lanigithub@users.noreply.github.com>
2022-06-08 12:22:34 -0700

    Add files via upload

---
+++ c#projects/IntroUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IntroLibrary;
+
+namespace IntroUI {
+    class Program {
+        static void Main(string[] args) {
+            PersonModel p = new PersonModel() {
+                FirstName = "Jenny",
+                LastName = "Johnson"
+            };
+            Console.WriteLine($"{p.FirstName} {p.LastName} is my name");
+
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a test");
+        }
+    }   
+}
+;
